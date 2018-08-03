e4ed523133f2676d199318e7a1fae93363b8bb69
Evgenia Zatonskaya <zatonskaya@yandex.ru>
2018-08-03 17:21:16 +0300

    Тест для групп переведен на pytest

--- test_add_group.py
+++ test_add_group.py
@@ -1,32 +1,24 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from group import Group
 from application import Applicaton
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.app = Applicaton()
+@pytest.fixture()
+def app(request):
+    fixture = Applicaton()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="newGroup", header="my group", footer="mygroup"))
-        self.app.logout()
 
-    def test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logout()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="newGroup", header="my group", footer="mygroup"))
+    app.logout()
 
-    def tearDown(self):
-        self.app.destroy()
 
-if __name__ == '__main__':
-    unittest.main()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
+
