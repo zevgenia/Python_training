dc631b5032b9d97a1246b0b4e88466bbbace2294
Evgenia Zatonskaya <zatonskaya@yandex.ru>
2018-08-07 13:21:28 +0300

    Созданы пакеты для разных зон ответственности

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from contact import Contact
-from application import Applicaton
+from model.contact import Contact
+from fixture.application import Applicaton
 
 
 @pytest.fixture()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application import Applicaton
+from model.group import Group
+from fixture.application import Applicaton
 
 
 @pytest.fixture()
