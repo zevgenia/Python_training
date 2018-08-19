64c70a8b82e7c10e3c58cd568dcefbe622bf8c1b
zevgenia <zatonskaya@yandex.ru>
2018-08-19 19:12:16 +0300

    Добавлена проверка списков по содержимому в тесты удаления групп и контактов

--- model/contact.py
+++ model/contact.py
@@ -4,4 +4,10 @@ class Contact:
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
+        self.id = id
 
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.lastname)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.lastname == other.lastname
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -8,3 +8,5 @@ def test_delete_first_contact(app):
     app.contact.delete_first_contact()
     new_contacts = app.contact.get_contacts_list()
     assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts[0:1] = []
+    assert old_contacts == new_contacts
