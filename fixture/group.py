50f8e5ee0ca200002c4b8ad01dabfb550a8942be
Evgenia Zatonskaya <zatonskaya@yandex.ru>
2018-08-22 15:29:08 +0300

    Реализована модификация случайной группы(по случайному индексу)

--- fixture/group.py
+++ fixture/group.py
@@ -29,10 +29,13 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cash = None
 
-    def modify_first_group(self, new_group_data):
+    def modify_first_group(self):
+        self.modify_group_by_index(0)
+
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # open form for modification
         wd.find_element_by_name("edit").click()
         self.fill_group_form(new_group_data)
