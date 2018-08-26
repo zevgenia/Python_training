e94451c62838169cd046d83c3fbf0b3349c2ceb7
zevgenia <zatonskaya@yandex.ru>
2018-08-26 20:42:49 +0300

    В тесте сравнения телефонов home_page-edit_page применен метод обратных проверок

--- fixture/contact.py
+++ fixture/contact.py
@@ -81,16 +81,9 @@ class ContactHelper:
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
                 lastname = cells[1].text
                 firstname = cells[2].text
-                all_phones = cells[5].text.splitlines()
-                print(all_phones[0])
-                print(all_phones[1])
-                print(all_phones[2])
-                print(all_phones[3])
+                all_phones = cells[5].text
                 self.contact_cash.append(Contact(lastname=lastname, firstname=firstname, id=id,
-                                                 homephone=all_phones[0],
-                                                 mobilephone=all_phones[1],
-                                                 workphone=all_phones[2],
-                                                 secondaryphone=all_phones[3]))
+                                                 all_phones_from_home_page=all_phones))
         return list(self.contact_cash)
 
     # откытие страницы редактирования контакта
